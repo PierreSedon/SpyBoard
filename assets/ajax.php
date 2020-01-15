cebc89611641c225985d555f1e7bd04c66a8826a
Pierre <pierre.sedon@outlook.fr>
2020-01-15 13:43:43 +0100

    modif script lancement + creation fichier pour dl

--- assets/ajax.php
+++ assets/ajax.php
@@ -10,37 +10,43 @@ if (isset($_POST['action'])) {
     switch($_POST['action']){
         case "logs":
             echo file_get_contents($outputfile);
-            break;
+        break;
         case "write":
             if (isset($_POST['command']) && strcmp($_POST['command'], "") != 0) {
                 $newfile = $outputfile . "-old";
                 if (!copy($outputfile, $newfile)) {
                     echo "La copie $file du fichier a échoué...\n";
                 }
-
+                
                 clearstatcache();
                 $mtime = filemtime($outputfile);
                 exec('echo "' . $_POST['command']. '" > command');
-
+                
                 $sleepcount = 0;
                 clearstatcache();
-
+                
                 while ($mtime == filemtime($outputfile) && ($sleepcount < $maxsleep)){
                     $sleepcount ++;
                     sleep(1);
                     clearstatcache();
                 }
-
+                
                 if ($sleepcount != $maxsleep){  
                     // Faire le diff entre fichiers et le retourner
                     $fulloutput = file_get_contents($outputfile);
                     $oldoutput = file_get_contents($newfile);
-                    echo str_replace($oldoutput, "", $fulloutput);
+                    $result = str_replace($oldoutput, "", $fulloutput);
+                    echo $result;
+                    // $my_file = '/tmp/savedfile.odt';
+                    // $handle = fopen($my_file, 'w+') or die('Cannot open file:  '.$my_file);
+                    // fwrite($handle, $result);
+                    // fclose($handle);
+                    // echo "saving file";
                 } else {
                     echo "Shell took too long to respond";
                 }
             }
-            break;
+        break;
     }
 }
 ?>
--- startreverse.sh
+++ startreverse.sh
@@ -4,5 +4,7 @@ mkfifo /tmp/reverseinput
 chmod a+w /tmp/reverseinput
 touch reverseresult
 chmod 777 reverseresult
+chmod o+w assets
+chmod o+w .
 ./feedpipe.sh &
 tail -f /tmp/reverseinput | nc -lvp 4242 > reverseresult
